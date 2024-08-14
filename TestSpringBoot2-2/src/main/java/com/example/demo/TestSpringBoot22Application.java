f2f4409bba7c72a567cc5ed53432d91bb856a832
SAI\maree <sai.mareedu@gmail.com>
2024-08-14 16:29:44 -0500

    Demo

---
+++ TestSpringBoot2-2/src/main/java/com/example/demo/TestSpringBoot22Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpringBoot22Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSpringBoot22Application.class, args);
+	}
+
+}
